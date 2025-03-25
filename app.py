92b059a1bd14b57ab03c4fc00cabbcdd7d564cac
A_Nama <145112688+A-Nama@users.noreply.github.com>
2025-03-25 16:28:49 +0400

    Updated backend URL in app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # Backend API URL
-API_BASE_URL = "http://127.0.0.1:8000"
+API_BASE_URL = "https://backendboba.onrender.com/"
 
 # Session state to manage authentication
 if "user" not in st.session_state:
